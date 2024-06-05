7db1b03b51c1508ff6675742f904f1b97c1bd2a8
Phanun29 <nenzigaming@gmail.com>
2024-06-05 16:15:50 +0700

    haha

--- helpdesk/Backend/index.php
+++ helpdesk/Backend/index.php
@@ -1,16 +1,15 @@
+<?php require('header.php'); ?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
 
-<?php require('header.php');?>
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-   
 
-    <!-- Main content -->
-    <!-- dashboadr -->
-    <section class="content">
-      
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  <?php require('footer.php');?>
-  <?php require('sidebar.php');?>
+  <!-- Main content -->
+  <!-- dashboadr -->
+  <section class="content">
+
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<?php require('footer.php'); ?>
+<?php require('sidebar.php'); ?>
--- helpdesk/Backend/sidebar.php
+++ helpdesk/Backend/sidebar.php
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <div class="brand-link">
-      <img src="<?php asset('dist/img/images (1).png');?>" alt="ptt cambodia" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="<?php asset('dist/img/images (1).png'); ?>" alt="ptt cambodia" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">HelpDesk</span>
     </div>
 
@@ -24,7 +24,7 @@
 
           <li class="nav-item">
             <a href="" class="nav-link">
-            <ion-icon name="ticket-outline"></ion-icon>
+              <ion-icon name="ticket-outline"></ion-icon>
               <p>
                 Layout Options
               </p>
